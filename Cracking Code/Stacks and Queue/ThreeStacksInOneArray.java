c90286040ff236c5c30c2f39a36cb61e0860a1f0
zain-raza-16 <truezainraza@outlook.com>
2021-01-23 18:19:14 +0500

    Question to do about three stacks in one array , just intial idea, try to do it with pointers

---
+++ Cracking Code/Stacks and Queue/ThreeStacksInOneArray.java
@@ -0,0 +1,15 @@
+public class ThreeStacksInOneArray {
+    int size;
+    int[][] theArray;
+
+    public ThreeStacksInOneArray(int numberOfStacks , int sizeOfSingleStack){
+        theArray = new int[numberOfStacks][sizeOfSingleStack];
+    }
+
+    public void addToStack(int stackNumber , int value){
+        // do a for loop for that particular array and add a value
+        theArray.
+    }
+
+    public void popFromStack(int stackNumber )
+}
