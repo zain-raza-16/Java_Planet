a076e88e169397c1be92346384c67627d8546d8f
zain-raza-16 <truezainraza@outlook.com>
2021-01-23 13:27:17 +0500

    Add code to the file

--- Cracking Code/Linked List/Palindrome.java
+++ Cracking Code/Linked List/Palindrome.java
@@ -1,1 +1,22 @@
+class Solution_1 {
 
+    public boolean palindrome(LinkedListNode input) {
+        LinkedListNode first = null;
+
+        first = input;
+        while (true) {
+            input = input.next;
+            if (input.next == null) {
+                break;
+            }
+        }
+
+        do {
+            if (first.data != input.data) return false;
+            first = first.next;
+            input = input.prev;
+        } while (first == input);
+
+        return true;
+    }
+}
